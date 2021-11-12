544d0466850e8ce3b8fa0796bc36329e383578e3
Will Holmes <willholmes60@gmail.com>
2021-11-12 16:21:16 +0000

    added dotenv for cloudinary

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,26 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-import "bootstrap";
-import { initSelect2 } from '../components/init_select2';
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-document.addEventListener("turbolinks:load", function () {
-  initSelect2();
-});
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+// Bootstrap import
+import "bootstrap";
+
+// Stimulus import
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+// Stimulus
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,3 +30,5 @@ document.addEventListener("turbolinks:load", function () {
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
