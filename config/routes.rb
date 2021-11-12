8fab31a5b9bf7ed6e4d595608f8558caded176fe
Will Holmes <willholmes60@gmail.com>
2021-11-12 12:00:57 +0000

    heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: 'list'
+
+  # get 'lists/:list_id/bookmarks', to: 'bookmarks#index'
+  # get 'lists/:list_id/bookmarks/new', to:
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts 'removing old database'
+
+Movie.destroy_all
+
+puts 'accessing the API'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies = JSON.parse(URI.open(url).read)
+
+puts "here it is #{movies}"
+
+puts 'building the database'
+movies['results'].each do |movie|
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+  puts "added the movie titled #{movie['title']}"
+end
+
+puts 'DONE!'
